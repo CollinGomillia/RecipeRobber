d050296767d5bbf5f81d9bbc197d2d6d5e5d9fc0
CollinGomillia <collinlgomillia@gmail.com>
2022-03-29 21:22:48 -0400

    Add project files.

---
+++ RecipeRobber.Data/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeRobber.Data
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        public int MakeTime { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset? ModifiedAt { get; set; }
+    }
+}
---
+++ RecipeRobber.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecipeRobber.MVC.MvcApplication" Language="C#" %>
